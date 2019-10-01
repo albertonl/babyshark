7639cfc4f589e85555394e9e9b48c4e8d4728a32
Andi Qu <dolphingarlic@gmail.com>
2019-10-01 18:03:12 +0200

    Improved the C++ implementation and fixed typos

--- babyshark.cpp
+++ babyshark.cpp
@@ -1,24 +1,14 @@
 #include <iostream>
-#include <vector>
 
-using namespace std;
-
-int main(){
-  vector<string> family = {"Baby", "Mommy", "Daddy", "Grandma", "Grandpa", "Let's go hunt", "Run away", "Safe at last", "It's the end"};
-  // The last four aren't actually part of a "family" but I didn't want to put them in another vector
-  for(int i=0;i<9;i++){
-    for(int j=0;j<3;j++){
-    	cout<<family[i];
-    	if(i<=4){
-    		cout<<" shark";
-    	}
-      cout<<", doo doo doo doo doo doo"<<endl;
+int main() {
+    std::string family[9]{"Baby shark",    "Mommy shark",   "Daddy shark",
+                          "Grandma shark", "Grandpa shark", "Let's go hunt",
+                          "Run away",      "Safe at last",  "It's the end"};
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 3; j++) {
+            std::cout << family[i] << ", doo doo doo doo doo doo\n";
+        }
+        std::cout << family[i] << "!\n";
     }
-    cout<<family[i];
-    if(i<=4){
-    	cout<<" shark";
-    }
-    cout<<"!"<<endl;
-  }
-  return 0;
+    return 0;
 }
