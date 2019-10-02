99e42ea8e16ed8f74bee1d84ffa6fdf5b7751831
Sayam Kanwar <sayamkanwar616@gmail.com>
2019-10-02 13:54:42 +0530

    Create babyshark.php

---
+++ babyshark.php
@@ -0,0 +1,9 @@
+<?php
+$keywords = array("Baby shark", "Mommy shark", "Daddy shark", "Grandma shark", "Grandpa shark", "Let's go hunt", "Run away", "Safe at last", "It's the end");
+foreach ($keywords as $key) {
+    for ($i=0; $i<3; $i++) {
+        echo $key . ", " . "doo doo doo doo doo doo\n";
+    }
+    echo $key . "!\n";
+}
+?>
